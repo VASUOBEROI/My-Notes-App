855e994f1fdb672c5e260e2bb94414cc4b724ce6
Vasu Oberoi <Vasu.Oberoi@ivanti.com>
2023-03-20 23:55:42 +0530

    Added-Templating Engine - Ejs

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const path=require('path');  // node core module.
 
-const express=require('express');         //dev-dependencies
+const express=require('express');         //prod-dependencies
 const bodyParser=require('body-parser'); //prod-dependencies.
 
 const userRouter=require('./routes/user');
@@ -8,6 +8,13 @@ const userRouter=require('./routes/user');
 
 
 const app=express(); 
+
+// Using Templating-Engine to Return Dynamic Data to Users.Exposing the Views directory as well.
+app.set('view engine','ejs');
+app.set('views','views');
+
+
+
 
 // Middleware to Parse Incoming Request.
 app.use(bodyParser.urlencoded({extended:false}));
@@ -20,7 +27,7 @@ app.use('/user',userRouter.userRouter);
 
 // All routes starting with /. 
 app.use('/',(req,res,next)=>{
-res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+res.status(404).render('404.ejs',{pageTitle:'Page Not Found Baby'});
 });
 
 
--- routes/user.js
+++ routes/user.js
@@ -5,19 +5,24 @@ const rootDir=require('../utils/path'); // importing exports from file.
 const express=require('express'); // Prod-Dependency
 const router=express.Router();     
 
+// /user/ ==>Get
 router.get('/',(req,res,next)=>{
-res.sendFile(path.join(rootDir,'views','/userMainPage.html'));
+res.render('userMainPage',{pageTitle:'User-Main-Page'});
 
 });
 
+//     /user/newNote/ ==>Get
 router.get('/new-Note',(req,res,next)=>{
- res.sendFile(path.join(rootDir,'views','newNote.html'));
+ res.render('newNote',{pageTitle:'New Note'});
 });
 
+
+//   /user/all-Notes ==> Get
 router.get('/all-Notes',(req,res,next)=>{
 res.send('<h1>Hi User, here are your Notes </h1>');
 });
 
+//   /user/all-Notes ==>Post
 router.post('/all-Notes',(req,res,next)=>{
 console.log(req.body);
 res.redirect('/user/all-Notes');
