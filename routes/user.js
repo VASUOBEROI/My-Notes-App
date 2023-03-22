dceba253b4a177987afdecee669549de1563f10c
Vasu Oberoi <Vasu.Oberoi@ivanti.com>
2023-03-23 00:19:39 +0530

    Added Styling to New Note,All-Notes

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,7 @@ const rootDir=require('../utils/path'); // importing exports from file.
 const express=require('express'); // Prod-Dependency
 const router=express.Router();     
 
+const notes=[];
 // user/ ==>Get
 router.get('/',(req,res,next)=>{
 res.render('landingPage.ejs',{pageTitle:'User-Main-Page',pageHeading:'Welcome User to My-Notes-App'});
@@ -19,17 +20,19 @@ router.get('/new-Note',(req,res,next)=>{
 
 //  user/all-Notes ==> Get
 router.get('/all-Notes',(req,res,next)=>{
-res.send('<h1>Hi User, here are your Notes </h1>');
+res.render('allNotes.ejs',{pageTitle:'User-All-Notes',pageHeading:'Hi User Check Your Notes',allNotes:notes})
 });
 
 //   user/all-Notes ==>Post
 router.post('/all-Notes',(req,res,next)=>{
+    notes.push(req.body);
 console.log(req.body);
 res.redirect('/user/all-Notes');
 });
 
 
 module.exports={
-userRouter:router
+userRouter:router,
+userNotes:notes
 };
 
